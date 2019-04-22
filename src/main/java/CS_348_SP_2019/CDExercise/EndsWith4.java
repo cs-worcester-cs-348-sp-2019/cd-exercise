b7d7ddc9257063163448be77d668545efff92960
Hnguyen18@worcester.edu <hnguyen18@worcester.edu>
2019-04-22 13:03:17 -0400

    Added name

--- src/main/java/CS_348_SP_2019/CDExercise/EndsWith4.java
+++ src/main/java/CS_348_SP_2019/CDExercise/EndsWith4.java
@@ -1,14 +1,14 @@
 package CS_348_SP_2019.CDExercise;
 
 public class EndsWith4 {
-	
+
 	String names;
 
 	public EndsWith4()  {
 		names = "";
 		// add your name here in the form of a line that looks like:
 		// names+= "John Smith, ";
-		
+		names+= "Harry Nguyen";
 	}
 
 	@Override
